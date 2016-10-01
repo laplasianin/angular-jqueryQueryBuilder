d0561d35e4fc5aa157be3d73fb0c09347cc280a5
ilya Lebedyuk <laplasianin@gmail.com>
2016-10-02 00:28:40 +0300

    Initial commit

--- dist/AngularJqueryQueryBuilderDirective.js
+++ dist/AngularJqueryQueryBuilderDirective.js
@@ -1,69 +1,49 @@
-angular.module('angular-jquery-querybuilder').directive('queryBuilderr', [
+angular.module('angular-jquery-querybuilder', []).directive('queryBuilder', [
   function() {
     return {
       restrict: 'AE',
       scope: {
+        options: '=',
+        api: '=',
         builder: '='
       },
       link: function($scope, $element) {
-        var _getOptions, buildApi, getRulesOrDefault, init, launchBuilder, updateRules, viewToModelNotificator;
-        updateRules = function() {
+        var _updateRules, getRulesOrDefault, init, launchBuilder, setBuilder, viewToModelNotificator;
+        _updateRules = function() {
           return $element[0].queryBuilder.setRules(getRulesOrDefault());
         };
-        buildApi = function() {
-          if ($scope.builder.api) {
-            $scope.builder.api.isValid = function() {
-              return $element[0].queryBuilder.validate();
-            };
-            $scope.builder.api.getRules = function() {
-              return $element[0].queryBuilder.getRules();
-            };
-            $scope.builder.api.updateRules = updateRules;
-            $scope.builder.api.init = launchBuilder;
-            return $scope.builder.builder = launchBuilder;
+        setBuilder = function() {
+          $scope.builder = $element[0].queryBuilder;
+          if ($scope.api) {
+            return $scope.api.updateRules = _updateRules;
           }
         };
         viewToModelNotificator = function() {
           return $element.on('afterUpdateRuleValue.queryBuilder    afterUpdateRuleFilter.queryBuilder afterUpdateRuleOperator.queryBuilder afterUpdateGroupCondition.queryBuilder afterReset.queryBuilder              afterMove.queryBuilder afterSetFilters.queryBuilder         afterInvert.queryBuilder afterDeleteRule.queryBuilder         afterDeleteGroup.queryBuilder', function() {
             var newRules;
             newRules = $element[0].queryBuilder.getRules();
-            if ((!angular.equals(newRules, $scope.builder.rules)) && ((newRules != null ? newRules.condition : void 0) || (newRules != null ? newRules.rules : void 0))) {
+            if ((!angular.equals(newRules, $scope.options.rules)) && ((newRules != null ? newRules.condition : void 0) || (newRules != null ? newRules.rules : void 0))) {
               return $scope.$emit("QueryBuilderValueChanged");
             }
           });
         };
         getRulesOrDefault = function() {
           var ref, ref1;
-          if (!((ref = $scope.builder.rules) != null ? ref.condition : void 0) && !((ref1 = $scope.builder.rules) != null ? ref1.rules : void 0)) {
+          if (!((ref = $scope.options.rules) != null ? ref.condition : void 0) && !((ref1 = $scope.options.rules) != null ? ref1.rules : void 0)) {
             return {
               condition: "OR",
               rules: []
             };
           } else {
-            return $scope.builder.rules;
+            return $scope.options.rules;
           }
-        };
-        _getOptions = function() {
-          var option, options, ref, value;
-          options = {
-            filters: $scope.builder.filters,
-            rules: getRulesOrDefault()
-          };
-          if ($scope.builder.config) {
-            ref = $scope.builder.config;
-            for (option in ref) {
-              value = ref[option];
-              options[option] = value;
-            }
-          }
-          return options;
         };
         launchBuilder = function() {
-          return $element.queryBuilder(_getOptions());
+          return $element.queryBuilder($scope.options);
         };
         init = function() {
           launchBuilder();
-          buildApi();
+          setBuilder();
           return viewToModelNotificator();
         };
         return init();
--- src/AngularJqueryQueryBuilderDirective.coffee
+++ src/AngularJqueryQueryBuilderDirective.coffee
@@ -1,25 +1,20 @@
 angular.module 'angular-jquery-querybuilder', []
-.directive 'queryBuilderr', [ () ->
+.directive 'queryBuilder', [ () ->
   restrict: 'AE'
   scope:
+    options: '='
+    api: '='
     builder: '='
-    # .rules  required
-    # .filters  required
-    # .config 
-    # .api
 
   link: ($scope, $element) ->
 
-    updateRules = ->
+    _updateRules = ->
       $element[0].queryBuilder.setRules(getRulesOrDefault())
 
-    buildApi = ->
-      if $scope.builder.api
-        $scope.builder.api.isValid = -> $element[0].queryBuilder.validate()
-        $scope.builder.api.getRules = -> $element[0].queryBuilder.getRules()
-        $scope.builder.api.updateRules = updateRules
-        $scope.builder.api.init = launchBuilder
-        $scope.builder.builder = $element[0].queryBuilder
+    setBuilder = ->
+      $scope.builder = $element[0].queryBuilder
+      if $scope.api
+        $scope.api.updateRules = _updateRules
 
     viewToModelNotificator = ->
       $element.on 'afterUpdateRuleValue.queryBuilder    afterUpdateRuleFilter.queryBuilder
@@ -28,32 +23,21 @@ angular.module 'angular-jquery-querybuilder', []
                    afterSetFilters.queryBuilder         afterInvert.queryBuilder
                    afterDeleteRule.queryBuilder         afterDeleteGroup.queryBuilder', ->  # these are all events which could change rules value
         newRules = $element[0].queryBuilder.getRules()
-        if (not angular.equals newRules, $scope.builder.rules) and (newRules?.condition or newRules?.rules)
+        if (not angular.equals newRules, $scope.options.rules) and (newRules?.condition or newRules?.rules)
           $scope.$emit("QueryBuilderValueChanged")
 
     getRulesOrDefault = ->
-      if not $scope.builder.rules?.condition and not $scope.builder.rules?.rules # to save querybuilder from incorrect external values
+      if not $scope.options.rules?.condition and not $scope.options.rules?.rules # to save querybuilder from incorrect external values
         {condition: "OR", rules: []}
       else
-        $scope.builder.rules
-
-    _getOptions = ->
-      options =
-        filters: $scope.builder.filters
-        rules: getRulesOrDefault()
-
-      if $scope.builder.config
-        for option, value of $scope.builder.config
-          options[option] = value
-      options
+        $scope.options.rules
 
     launchBuilder = ->
-      $element.queryBuilder _getOptions()
+      $element.queryBuilder $scope.options
 
     init = ->
       launchBuilder()
-      buildApi()
+      setBuilder()
       viewToModelNotificator()
-
     do init
 ]
